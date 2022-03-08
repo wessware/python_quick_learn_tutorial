3ae8e6e819a318df3124d9a274479ce95e7bc8d9
wessware <stevenshisia@yahoo.com>
2022-03-08 06:50:41 +0300

    game logic done

--- black_jack_game/BJ_modularized/main_bj.py
+++ black_jack_game/BJ_modularized/main_bj.py
@@ -92,3 +92,71 @@ class BJ_Dealer(BJ_Hand):
     def flip_first_card(self):
         first_card = self.cards[0]
         first_card.flip()
+
+
+class BJ_Game(object):
+
+    def __init__(self, names):
+        self.players = []
+        for name in names:
+            player = BJ_Player(name)
+            self.players.append(player)
+
+        self.dealer = BJ_Dealer('Dealer')
+
+        self.deck = BJ_Deck()
+        self.deck.populate()
+        self.deck.shuffle()
+
+    @property
+    def still_playing(self):
+        sp = []
+        for player in self.players:
+            if not player.is_busted():
+                sp.append(player)
+
+        return sp
+
+    def __additional_cards(self, player):
+        while not player.is_busted() and player.is_hitting():
+            self.deck.deal([player])
+
+            print(player)
+            if player.is_busted():
+                player.bust()
+
+    def play(self):
+        self.deck.deal(self.players + [self.dealer], per_hand=2)
+        self.dealer.flip_first_card()
+        for player in self.players:
+            print(player)
+        print(self.dealer)
+
+        for player in self.players:
+            self.__additional_cards(player)
+
+        self.dealer.flip_first_card()
+
+        if not self.still_playing:
+            print(self.dealer)
+
+        else:
+            print(self.dealer)
+            self.__additional_cards(self.dealer)
+
+            if self.dealer.is_busted():
+
+                for player in self.still_playing:
+                    player.win()
+            else:
+                for player in self.still_playing:
+                    if player.total > self.dealer.total:
+                        player.win()
+                    elif player.total < self.dealer.total:
+                        player.lose()
+                    else:
+                        player.push()
+
+        for player in self.players:
+            player.clear()
+        self.dealer.clear()
